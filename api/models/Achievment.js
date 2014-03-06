866991dc6324dd08891032ba4dc620504a547a14
Igor Marques da Silva <igormarquessilva@gmail.com>
2014-03-06 17:42:34 -0300

    Generate achievment model. #2

---
+++ api/models/Achievment.js
@@ -0,0 +1,22 @@
+/**
+ * Achievment
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    title: 'string',
+    description: 'string',
+    game: 'integer',
+    icon: 'string',
+  }
+
+};
